d2470070b6383a4610e32cb1411f504e8e2c0df7
zhangdongrun_dxm <zhangdongrun@duxiaoman.com>
2024-01-14 12:04:35 +0800

    add 90

--- src/main/java/com/example/demo/basetype/BackTrack.java
+++ src/main/java/com/example/demo/basetype/BackTrack.java
@@ -1,6 +1,7 @@
 package com.example.demo.basetype;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -29,5 +30,57 @@ public class BackTrack {
             subList.remove(subList.size() - 1);
         }
     }
+
+    // 90. Subsets II
+    // https://leetcode.com/problems/subsets-ii/description/
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> subList = new ArrayList<>();
+        Arrays.sort(nums);
+        deepSubsetsWithDup(nums, 0, result, subList);
+        return result;
+    }
+
+    public static void deepSubsetsWithDup(int[] nums, int start, List<List<Integer>> result, List<Integer> subList) {
+        result.add(new ArrayList<>(subList));
+        for (int i = start; i< nums.length; ++i) {
+            // 跳过
+            if (i != start && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            subList.add(nums[i]);
+            deepSubsetsWithDup(nums, i + 1, result, subList);
+            subList.remove(subList.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 2};
+        System.out.println(subsetsWithDup(nums));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
