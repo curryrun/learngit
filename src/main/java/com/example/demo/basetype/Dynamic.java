977822e53acb2d9ea6f790829e4dad36f1113b7e
zhangdongrun_dxm <zhangdongrun@duxiaoman.com>
2024-01-25 10:13:57 +0800

    init dynamic

---
+++ src/main/java/com/example/demo/basetype/Dynamic.java
@@ -0,0 +1,15 @@
+package com.example.demo.basetype;
+
+/**
+ * @Classname Dynamic
+ * @Description
+ * @Date 2024/1/24 22:06
+ * @Creater zhangdongrun_dxm
+ *
+ * 动规是由前一个状态推导出来的，而贪心是局部直接选最优的
+ * 动态规划
+ * Dynamic Programming，简称DP
+ *
+ **/
+public class Dynamic {
+}
